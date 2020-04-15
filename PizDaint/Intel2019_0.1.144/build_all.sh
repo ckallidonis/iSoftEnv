5fca66ff30ac6338ad44c733f8f0c6c677c9e96c
ckallidonis <kallidonis@gmail.com>
2020-04-15 17:08:53 +0200

    Script to build all packages in the right order, using Intel-19.0.1.144 on Piz Daint

---
+++ PizDaint/Intel2019_0.1.144/build_all.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+
+prog=qmp
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=qdpxx
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=qphix
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=mgproto
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=chroma
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=tensor
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=hadron
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+ 
+prog=chroma-devel-genprop
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=adat
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=adat-devel-pdf
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=colorvec
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=colorvec-devel-pdf
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=qdpxx_3d
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=harom
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=redstar
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+sleep 2
+
+prog=redstar-devel-pdf
+echo -e "\n##### Building ${prog} #####\n"
+./build_${prog}.sh
+echo -e "\n##### ${prog} DONE #####\n"
+
+
+echo -e "\n\n##### Building all packages completed! #####\n\n"
