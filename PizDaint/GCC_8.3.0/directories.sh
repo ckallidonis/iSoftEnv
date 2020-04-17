1682a733b23f97640b45cbe1355336f3780a83ab
ckallidonis <kallidonis@gmail.com>
2020-04-17 18:58:38 +0200

    Added directories for Chroma and QUDA for GNU builds on Piz Daint

--- PizDaint/GCC_8.3.0/directories.sh
+++ PizDaint/GCC_8.3.0/directories.sh
@@ -5,8 +5,10 @@ BUILD_DIST=GCC_8.3.0
 # Package distribution names
 QMP_DIST=qmp
 LLVM_DIST=llvm-8.0.0
-QDPJIT_DIST=qdp-jit
-#QDPJIT_DIST=qdp-jit-llvm-nvptx
+QDPJIT_DIST=qdp-jit-llvm-nvptx
+#QDPJIT_DIST=qdp-jit
+CHROMA_DIST=chroma
+QUDA_DIST=quda
 
 # Generic directories
 TOPDIR=/users/kallidoc/software
@@ -25,6 +27,13 @@ LLVM_INSTALL=${INSTALL_TOP}/${LLVM_DIST}
 QDPJIT_SRC=${SRC_TOP}/${QDPJIT_DIST}
 QDPJIT_INSTALL=${INSTALL_TOP}/${QDPJIT_DIST}
 
+# CHROMA directories
+CHROMA_SRC=${SRC_TOP}/${CHROMA_DIST}_gnu
+CHROMA_INSTALL=${INSTALL_TOP}/${CHROMA_DIST}
+
+# QUDA directories
+QUDA_SRC=${SRC_TOP}/${QUDA_DIST}
+QUDA_INSTALL=${INSTALL_TOP}/${QUDA_DIST}
 
 # Build files directory
 BUILDDIR=${INSTALL_TOP}/build_files
