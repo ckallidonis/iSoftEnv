a15e17c97f73fa305585b38daab860a98506919e
ckallidonis <kallidonis@gmail.com>
2020-04-20 22:25:35 +0200

    Updated script for qdp-jit to allow for packing/unpacking of math functions in CUDA libdevice

--- PizDaint/GCC_8.3.0/build_qdp_jit.sh
+++ PizDaint/GCC_8.3.0/build_qdp_jit.sh
@@ -18,25 +18,25 @@ PKG_DIST=${QDPJIT_DIST}_${BUILD_TAG}
 
 pushd ${PKG_SRC}
 
-#echo "Cleaning QDP-JIT LibDeviceSipport"
-#rm -rf ./libdevice_files
-#rm -f ./include/qdp_libdevice.h
-#rm -f ./lib/qdp_libdevice.cc
-#echo "QDP-JIT adding LibDevice patch"
+echo "Cleaning QDP-JIT LibDeviceSipport"
+rm -rf ./libdevice_files
+rm -f ./include/qdp_libdevice.h
+rm -f ./lib/qdp_libdevice.cc
+echo "QDP-JIT adding LibDevice patch"
 mkdir -p ./libdevice_files
 
-#case ${SM} in
-#sm_30|sm_35)
-#  echo "SM30 or SM35 Applying Patch for SQRT nonsense" 
-#  cd ./libdevice_files
-#  ../patch_libdevice.sh ${PK_CUDA_HOME}/nvvm/libdevice  ${LLVM_INSTALL_DIR}
-#  cd ..
-#;;
-#*)
-# echo "Not SM30 nor SM35. Copying LibDevice Files"
-#  cp ${PK_CUDA_HOME}/nvvm/libdevice/* ./libdevice_files
-#;;
-#esac
+case ${SM} in
+sm_30|sm_35)
+ echo "SM30 or SM35 Applying Patch for SQRT nonsense" 
+ cd ./libdevice_files
+ ../patch_libdevice.sh ${PK_CUDA_HOME}/nvvm/libdevice  ${LLVM_INSTALL_DIR}
+ cd ..
+;;
+*)
+echo "Not SM30 nor SM35. Copying LibDevice Files"
+ cp ${PK_CUDA_HOME}/nvvm/libdevice/* ./libdevice_files
+;;
+esac
 
 echo "After potential patching libdevice_files contains the following:"
 ls ./libdevice_files
@@ -47,43 +47,43 @@ echo "Moving the right libdevice file to libdevice.bc"
 pushd ./libdevice_files
 
 CUDA_MAJOR=`nvcc --version | grep release | awk '{ print $6}' | cut -f2 -dV | cut -f1 -d.`
-#case ${CUDA_MAJOR} in
-#7|8)
-#      echo CUDA v7 or 8.
-#      case ${SM} in
-#      sm_30)
-#        LIBDEVICE_FILE="libdevice.compute_30.10.bc"
-#        ;;
-#      sm_35)
-#        # Use SM_35 Libdevice file
-#        LIBDEVICE_FILE="libdevice.compute_35.10.bc"
-#        ;;
-#      sm_37)
-#        # Use SM_35 Libdevice file
-#        LIBDEVICE_FILE="libdevice.compute_35.10.bc"
-#        ;;
-#      sm_5*)
-        # Use SM_50 libdevice file
-#        LIBDEVICE_FILE="libdevice.compute_50.10.bc"
-#        ;;
-#      sm_6*)
-        # Use SM_30 Libdevice file for SM_60 (Kate told me this)
-#        LIBDEVICE_FILE="libdevice.compute_30.10.bc"
-#        ;;
-#      *)
-#        echo "That I dont know which libdevice file to use for that ${SM}"
-#        echo "For SMs more recent than SM_60, please use CUDA-9 or better"
-#        exit -1
-#        ;;
-#      esac
-#      echo "SM is ${SM} : Copying ${LIBDEVICE_FILE} to ./libdevice.bc"
-#      cp ${LIBDEVICE_FILE} libdevice.bc
-#      ;;
-#*) 
-#      echo CUDA v9. Copying libdevice.10.bc to libdevice.bc
-#      mv libdevice.10.bc libdevice.bc
-#      ;;
-#esac
+case ${CUDA_MAJOR} in
+7|8)
+     echo CUDA v7 or 8.
+     case ${SM} in
+     sm_30)
+       LIBDEVICE_FILE="libdevice.compute_30.10.bc"
+       ;;
+     sm_35)
+       # Use SM_35 Libdevice file
+       LIBDEVICE_FILE="libdevice.compute_35.10.bc"
+       ;;
+     sm_37)
+       # Use SM_35 Libdevice file
+       LIBDEVICE_FILE="libdevice.compute_35.10.bc"
+       ;;
+     sm_5*)
+       # Use SM_50 libdevice file
+       LIBDEVICE_FILE="libdevice.compute_50.10.bc"
+       ;;
+     sm_6*)
+       # Use SM_30 Libdevice file for SM_60 (Kate told me this)
+       LIBDEVICE_FILE="libdevice.compute_30.10.bc"
+       ;;
+     *)
+       echo "That I dont know which libdevice file to use for that ${SM}"
+       echo "For SMs more recent than SM_60, please use CUDA-9 or better"
+       exit -1
+       ;;
+     esac
+     echo "SM is ${SM} : Copying ${LIBDEVICE_FILE} to ./libdevice.bc"
+     cp ${LIBDEVICE_FILE} libdevice.bc
+     ;;
+*) 
+     echo CUDA v9. Copying libdevice.10.bc to libdevice.bc
+     mv libdevice.10.bc libdevice.bc
+     ;;
+esac
 popd ## libdevice_files
 
 ./pack_libdevice.sh ./libdevice_files
@@ -126,6 +126,7 @@ mkdir -p ${PKG_INSTALL}
 
 ${PKG_SRC}/configure \
 	  --prefix=${PKG_INSTALL} \
+	  --with-libxml2=${LIBXML2_INSTALL} \
 	  --with-qmp=${QMP_INSTALL} \
 	  --enable-comm-split-deviceinit \
           --enable-parallel-arch=parscalar \
