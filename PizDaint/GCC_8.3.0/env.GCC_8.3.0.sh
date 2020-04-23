393b78fd74b94ca7a8f1ba7cda8b7edcf00a7456
ckallidonis <kallidonis@gmail.com>
2020-04-23 18:37:10 +0200

    Updated environment for Piz Daint using GCC-8.3.0

--- PizDaint/GCC_8.3.0/env.GCC_8.3.0.sh
+++ PizDaint/GCC_8.3.0/env.GCC_8.3.0.sh
@@ -20,9 +20,10 @@ export PK_CFLAGS=${OMPFLAGS}"-g -O3 -std=c99 "
 
 # Compilers for compiling package (passed as CC to ./configure throghout)
 # cc and CC are drivers/wrappers that point to the correct compilers based on the Programming Environment loaded
+GNU_GCC_DIR=/opt/gcc/8.3.0/snos
+export CC_LIBS=${GNU_GCC_DIR}/lib64
+
 export PK_FF="/opt/gcc/8.3.0/bin/gfortran"
-export PK_CC_SERIAL="/opt/gcc/8.3.0/bin/gcc"
-export PK_CXX_SERIAL="/opt/gcc/8.3.0/bin/g++"
 
 export PK_CC="cc"
 export PK_CXX="CC"
@@ -34,27 +35,24 @@ export PK_CMAKE=/users/kallidoc/software/install/GCC_8.3.0/cmake-3.17.1/bin/cmak
 export PK_CUDA_HOME=${CUDATOOLKIT_HOME}
 export SM=sm_60
 
-# GNU / CRAY-MPICH directories
-export CRAY_MPI_LIBS=$CRAY_MPICH2_DIR/lib
-export CC_LIBS=/opt/gcc/8.3.0/snos/lib64
 
 # PATH / LD_LIBRARY_PATH
 export PATH=${LLVM_INSTALL}/bin:/opt/gcc/8.3.0/snos/bin:${PATH}
-export LD_LIBRARY_PATH=${LLVM_INSTALL}/lib:${PK_CUDA_HOME}/nvvm/lib64:${CC_LIBS}:${CRAY_MPI_LIBS}:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${LLVM_INSTALL}/lib:${PK_CUDA_HOME}/nvvm/lib64:${CC_LIBS}:${LD_LIBRARY_PATH}
 echo $LD_LIBRARY_PATH
 
+
 #LLVM
+export PK_CC_LLVM="${GNU_GCC_DIR}/bin/gcc"
+export PK_CXX_LLVM="${GNU_GCC_DIR}/bin/g++"
 export LLVM_CXXFLAGS="-O3 -std=c++11"
 export LLVM_CFLAGS="-O3 -std=c99"
 
 
 # QDP-JIT related variables
-export QDPJIT_LDFLAGS="${OMPFLAGS} -L${CRAY_MPI_LIBS} -L${LLVM_INSTALL}/lib -L${PK_CUDA_HOME}/lib64 -L${PK_CUDA_HOME}/nvvm/lib64  -Wl,-rpath=${LLVM_INSTALL}/lib -Wl,-rpath=${PK_CUDA_HOME}/lib64 -Wl,-rpath=${PK_CUDA_HOME}/nvvm/lib64 -Wl,-rpath=${CRAY_MPI_LIBS} -Wl,-rpath=${CC_LIBS}"
-export QDPJIT_LIBS="-ldl -pthread -lmpich -lmpich_gnu_82"
-export QDPJIT_INC="-I${CRAY_MPICH2_DIR}/include"
-
-export CHROMA_LIBS="-lcublas -lcudart ${QDPJIT_LIBS}"
-export CHROMA_LDFLAGS="${QDPJIT_LDFLAGS}"
+export JIT_LDFLAGS="${OMPFLAGS} -L${LLVM_INSTALL}/lib -L${PK_CUDA_HOME}/lib64 -L${PK_CUDA_HOME}/nvvm/lib64  -Wl,-rpath=${LLVM_INSTALL}/lib -Wl,-rpath=${PK_CUDA_HOME}/lib64  -Wl,-rpath=${PK_CUDA_HOME}/nvvm/lib64 -Wl,-rpath=${CC_LIBS}"
+export JIT_LIBS="-ldl -pthread" # -lmpich -lmpich_gnu_82"
+export JIT_INC=""
 
 # Make
 PK_TARGET_JN="8"
