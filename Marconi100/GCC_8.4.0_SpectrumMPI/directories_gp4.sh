9a2efdebea7a4398f35f5044ab6b4148c9ef416e
Christos Kallidonis <kallidonis@gmail.com>
2020-11-18 18:08:58 +0100

    Updated directories for Genprop-4 builds with GNU GCC

--- Marconi100/GCC_8.4.0_SpectrumMPI/directories_gp4.sh
+++ Marconi100/GCC_8.4.0_SpectrumMPI/directories_gp4.sh
@@ -4,47 +4,44 @@ BUILD_DIST_GP4=GCC_8.4.0_SpectrumMPI
 
 
 # Package distribution names
-CHROMA_DIST_GP4=chroma-genprop4
-CHROMA_DIST_ORIG_GP4=chroma-genprop4-orig
+CHROMA_DIST_GP4=chroma_gp4
 QMP_DIST_GP4=qmp
 QDP_DIST_GP4=qdpxx
+LIBXML2_DIST_GP4=libxml2
 QDP3D_DIST_GP4=qdpxx_3d
 HADRON_DIST_GP4=hadron
-TENSOR_DIST_GP4=tensor
-HAROM_DIST_GP4=harom-genprop4
-QUDA_DIST_GP4=quda_genprop4
+HAROM_DIST_GP4=harom_gp4
+QUDA_DIST_GP4=quda_develop
 EIGEN_DIST_GP4=eigen-3.3.4
 
 # Generic directories
 TOPDIR_GP4=/m100_work/Pra21_5389/kallidoc/software
-SRC_TOP_GP4=${TOPDIR_GP4}/src_genprop4
-INSTALL_TOP_GP4=${TOPDIR_GP4}/install_genprop4/${BUILD_DIST_GP4}
+SRC_TOP_GP4=${TOPDIR_GP4}/src_gp4
+INSTALL_TOP_GP4=${TOPDIR_GP4}/install_gp4/${BUILD_DIST_GP4}
 
 
 # QMP directories
 QMP_SRC_GP4=${SRC_TOP_GP4}/${QMP_DIST_GP4}
 QMP_INSTALL_GP4=${INSTALL_TOP_GP4}/${QMP_DIST_GP4}
-QMP_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_genprop4/XL_16.1.1_SpectrumMPI/qmp
+
+# LIBXML2 directories
+LIBXML2_SRC_GP4=${SRC_TOP_GP4}/${LIBXML2_DIST_GP4}
+LIBXML2_INSTALL_GP4=${INSTALL_TOP_GP4}/${LIBXML2_DIST_GP4}
 
 # QDP directories
 QDP_SRC_GP4=${SRC_TOP_GP4}/${QDP_DIST_GP4}
 QDP_INSTALL_GP4=${INSTALL_TOP_GP4}/${QDP_DIST_GP4}
-QDP_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_genprop4/XL_16.1.1_SpectrumMPI/qdpxx
 
 # QDP-3D directories (Source code is same as QDP, installation is different)
 QDP3D_SRC_GP4=${SRC_TOP_GP4}/${QDP_DIST_GP4}
 QDP3D_INSTALL_GP4=${INSTALL_TOP_GP4}/${QDP3D_DIST_GP4}
-QDP3D_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_genprop4/XL_16.1.1_SpectrumMPI/qdpxx_3d
 
 # HADRON directories
 HADRON_SRC_GP4=${SRC_TOP_GP4}/${HADRON_DIST_GP4}
 HADRON_INSTALL_GP4=${INSTALL_TOP_GP4}/${HADRON_DIST_GP4}
-HADRON_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_genprop4/XL_16.1.1_SpectrumMPI/hadron
 
-# TENSOR directories
-TENSOR_SRC_GP4=${SRC_TOP_GP4}/${TENSOR_DIST_GP4}
-TENSOR_INSTALL_GP4=${INSTALL_TOP_GP4}/${TENSOR_DIST_GP4}
-TENSOR_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_genprop4/XL_16.1.1_SpectrumMPI/tensor
+# TENSOR directories, we are using XL/IBM build of Tensor!!
+TENSOR_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_gp4/XL_16.1.1_SpectrumMPI/tensor
 
 # CHROMA directories
 CHROMA_SRC_GP4=${SRC_TOP_GP4}/${CHROMA_DIST_GP4}
@@ -53,7 +50,6 @@ CHROMA_INSTALL_GP4=${INSTALL_TOP_GP4}/${CHROMA_DIST_GP4}
 # HAROM directories
 HAROM_SRC_GP4=${SRC_TOP_GP4}/${HAROM_DIST_GP4}
 HAROM_INSTALL_GP4=${INSTALL_TOP_GP4}/${HAROM_DIST_GP4}
-HAROM_INSTALL_XL_GP4=/m100_work/Pra21_5389/kallidoc/software/install_genprop4/XL_16.1.1_SpectrumMPI/harom
 
 # QUDA directories
 QUDA_SRC_GP4=${SRC_TOP_GP4}/${QUDA_DIST_GP4}
