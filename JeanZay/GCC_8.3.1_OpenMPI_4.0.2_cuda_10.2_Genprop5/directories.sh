99b97010dfaa19a95578ba94d74c97a76eec987d
Christos Kallidonis <kallidonis@gmail.com>
2021-04-19 22:44:41 +0200

    Directories file for building Genprop5 on Jean Zay

---
+++ JeanZay/GCC_8.3.1_OpenMPI_4.0.2_cuda_10.2_Genprop5/directories.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+
+BUILD_DIST=GCC_8.3.1_OpenMPI_4.0.2_cuda_10.2_Genprop5
+
+# Package distribution names
+QMP_DIST=qmp
+LIBXML2_DIST=libxml2
+LLVM_DIST=llvm
+QDPJIT_DIST=qdp-jit
+CHROMA_DIST=chroma2
+QUDA_DIST=quda_balint
+#QUDA_DIST=quda
+EIGEN_DIST=eigen
+OPENBLAS_DIST=openblas
+SUPERBBLAS_DIST=superbblas
+
+# Generic directories
+TOPDIR=/gpfswork/rech/ual/uie52up/software
+SRC_TOP=${TOPDIR}/src_gp5
+INSTALL_TOP=${TOPDIR}/install_gp5/${BUILD_DIST}
+
+# QMP directories
+QMP_SRC=${SRC_TOP}/${QMP_DIST}
+QMP_INSTALL_GNU=${INSTALL_TOP}/${QMP_DIST}
+
+# LLVM directories
+LLVM_SRC=${SRC_TOP}/${LLVM_DIST}
+LLVM_INSTALL=${INSTALL_TOP}/${LLVM_DIST}
+
+# LIBXML2 directories
+LIBXML2_SRC=${SRC_TOP}/${LIBXML2_DIST}
+LIBXML2_INSTALL_GNU=${INSTALL_TOP}/${LIBXML2_DIST}
+
+# QDP-JIT directories
+QDPJIT_SRC=${SRC_TOP}/${QDPJIT_DIST}
+QDPJIT_INSTALL=${INSTALL_TOP}/${QDPJIT_DIST}
+
+# CHROMA directories
+CHROMA_SRC=${SRC_TOP}/${CHROMA_DIST}
+CHROMA_INSTALL=${INSTALL_TOP}/${CHROMA_DIST}
+
+# QUDA directories
+QUDA_SRC=${SRC_TOP}/${QUDA_DIST}
+QUDA_INSTALL=${INSTALL_TOP}/${QUDA_DIST}
+
+# Eigen directories
+EIGEN_SRC=${SRC_TOP}/${EIGEN_DIST}
+
+# Openblas directories
+OPENBLAS_SRC=${SRC_TOP}/${OPENBLAS_DIST}
+OPENBLAS_INSTALL=${INSTALL_TOP}/${OPENBLAS_DIST}
+
+# Superbblas directories
+SUPERBBLAS_SRC=${SRC_TOP}/${SUPERBBLAS_DIST}
+SUPERBBLAS_INSTALL=${INSTALL_TOP}/${SUPERBBLAS_DIST}
+
+# Build files directory
+BUILDDIR=${INSTALL_TOP}/build_files
+mkdir -p ${BUILDDIR}
+
+
+export DIR_SETUP=1
